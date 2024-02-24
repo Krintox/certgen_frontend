73bd9ec20bdf5ef4ce06a616cc555b86d4bed074
unknown <alirashid.38@yahoo.com>
2024-02-24 16:33:39 +0530

    feature: added uploadpage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import UploadPage from './pages/UploadPage';
 import {UserContextProvider} from "./UserContext";
 import CreatePost from "./pages/addPhotos";
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost />} />
+          <Route path="/upload" element={<UploadPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
