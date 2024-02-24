5278bbe0cc165e1dc86a10f59faf0ca2ba49ca23
Krintox <shashanksuggala@gmail.com>
2024-02-25 02:42:07 +0530

    fix: added post routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Post from "./Post";
 import Header from "./Header";
 import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import {UserContextProvider} from "./UserContext";
-import CreatePost from "./pages/addPhotos";
+import CreatePost from "./pages/CreatePost";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <UserContextProvider>
       <Routes>
           <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost />} />
---
+++ src/pages/IndexPage.js
@@ -0,0 +1,10 @@
+import Post from "../Post";
+import {useEffect, useState} from "react";
+
+export default function IndexPage() {
+  return (
+    <>
+    This is the home page
+    </>
+  );
+}
