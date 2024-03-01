71d19d5cd2f42161ee346905601160f4e01805d3
SikariaKhushi <145835367+SikariaKhushi@users.noreply.github.com>
2024-03-01 16:27:33 +0000

    routing fixed

--- src/pages/Canvas.js
+++ src/pages/Canvas.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { fabric } from 'fabric';
 import Word from './Word';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Canvas = () => {
   const [canvas, setCanvas] = useState(null);
   const [annotations, setAnnotations] = useState([]);
   const [imageURL, setImageURL] = useState('');
   const [customText, setCustomText] = useState('');
+
+  const location = useLocation();
+  const uploadedImageFile = location.state ? location.state.uploadedImageFile : null;
+
+  useEffect(() => {
+    if (canvas && uploadedImageFile) {
+      loadImageToCanvas(uploadedImageFile);
+    }
+  }, [canvas, uploadedImageFile]);
+
+  useEffect(() => {
+    if (!canvas) {
+      const newCanvas = new fabric.Canvas('canvas', { width: 800, height: 600 });
+      setCanvas(newCanvas);
+    }
+  }, [canvas]);
 
   useEffect(() => {
     if (canvas) {
@@ -33,6 +51,23 @@ const Canvas = () => {
       });
     }
   }, [canvas, annotations]);
+
+  const loadImageToCanvas = (file) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.onload = () => {
+        const fabricImg = new fabric.Image(img, {
+          scaleX: canvas.width / img.width,
+          scaleY: canvas.height / img.height,
+        });
+        canvas.setBackgroundImage(fabricImg, canvas.renderAll.bind(canvas));
+        setImageURL(event.target.result);
+      };
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  };
 
   const handleDrop = (e) => {
     e.preventDefault();
@@ -107,12 +142,20 @@ const Canvas = () => {
     };
     reader.readAsDataURL(file);
   };
+  
+  const navigate = useNavigate();
+
+  const handleProceed = () => {
+    // Add logic here to customize the redirection as needed
+    navigate('/excel');
+  };
+  
 
   return (
     <div className="flex">
       <div className="w-1/3 p-4">
-        <h2>Words</h2>
-        <div id="words">
+        <h2>Wo<span className='text-orange-500'>rds</span></h2>
+        <div id="words" className='text-white'>
           <Word text="Word1" />
           <Word text="Word2" />
           {/* Add more words here */}
@@ -143,10 +186,13 @@ const Canvas = () => {
           <h2>Annotations</h2>
           <ul>
             {annotations.map((annotation, index) => (
-              <li key={index}>
+              <li key={index} className="text-white">
                 {annotation.word} - Left: {annotation.boundingBox.left}, Top: {annotation.boundingBox.top}, Width: {annotation.boundingBox.width}, Height: {annotation.boundingBox.height}, Font Size: {annotation.fontSize}
-                <button onClick={() => handleDeleteWord(annotation.word)}>Delete</button>
-              </li>
+              <div className='flex items-center pt-4'>
+              <button onClick={() => handleDeleteWord(annotation.word)} className="ml-2">Delete</button>
+              <button onClick={handleProceed} className="ml-2">Proceed</button>
+              </div>
+            </li>
             ))}
           </ul>
         </div>
--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,50 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const PreviewPage = () => {
-  const [excelData, setExcelData] = useState([]);
-
-  useEffect(() => {
-    // Parse the query parameter to extract excelData
-    const urlParams = new URLSearchParams(window.location.search);
-    const excelDataParam = urlParams.get('excelData');
-
-    if (excelDataParam) {
-      // Decode and parse the excelDataParam
-      const decodedExcelData = JSON.parse(decodeURIComponent(excelDataParam));
-      setExcelData(decodedExcelData);
-    }
-  }, []);
+  // Example hardcoded data
+  const hardcodedData = [
+    { slNo: 1, email: 'john@example.com', name: 'John Doe' },
+    { slNo: 2, email: 'jane@example.com', name: 'Jane Doe' },
+    { slNo: 3, email: 'jane@example.com', name: 'Jane Doe' },
+    { slNo: 4, email: 'jane@example.com', name: 'Jane Doe' },
+    // Add more data as needed
+  ];
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
       <h1 className="text-3xl md:text-5xl font-bold text-white border-b-2 under md:pb-2">CERT GEN - Preview</h1>
-      <div className="w-full max-w-md bg-transparent rounded-lg shadow-md mt-20">
-        {excelData.length > 0 && (
+      <div className="w-full max-w-2xl bg-transparent rounded-lg shadow-md mt-20">
+        {hardcodedData.length > 0 && (
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-4">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border-separate border border-white">
-              <thead className="text-xs text-white-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="w-full text-sm text-left rtl:text-right text-white border-separate border border-white">
+              <thead className="text-xs text-white uppercase">
                 <tr>
-                  <th scope="col" className="px-6 py-3 border-b border-r">
+                  <th scope="col" className="px-6 py-3 border-b border-r text-white">
                     SL No
                   </th>
-                  <th scope="col" className="px-6 py-3 border-b">
+                  <th scope="col" className="px-6 py-3 border-b text-white">
                     Email
                   </th>
-                  <th scope="col" className="px-6 py-3 border-b">
+                  <th scope="col" className="px-6 py-3 border-b text-white">
                     Name
                   </th>
-                  <th scope="col" className="px-6 py-3 border-b">
+                  <th scope="col" className="px-6 py-3 border-b text-white">
                     Click to Preview
                   </th>
                 </tr>
               </thead>
               <tbody>
-                {excelData.map((entry) => (
-                  <tr key={entry.slNo} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td className="px-6 py-4 border-b">{entry.slNo}</td>
-                    <td className="px-6 py-4 border-b">{entry.email}</td>
-                    <td className="px-6 py-4 border-b">{entry.name}</td>
-                    <td className="px-6 py-4 text-right border-b">
+                {hardcodedData.map((entry) => (
+                  <tr key={entry.slNo} className="border-b">
+                    <td className="px-6 py-4 text-white">{entry.slNo}</td>
+                    <td className="px-6 py-4 text-white">{entry.email}</td>
+                    <td className="px-6 py-4 text-white">{entry.name}</td>
+                    <td className="px-6 py-4 text-right">
                       <button className="hover:text-indigo-900 bg-green-600 p-2">Preview</button>
                     </td>
                   </tr>
@@ -53,6 +48,10 @@ const PreviewPage = () => {
             </table>
           </div>
         )}
+        <div className="flex justify-between mt-4 p-4">
+          <button className="bg-orange-500 text-white py-2 px-4 rounded mr-2">Download as ZIP format</button>
+          <button className="bg-orange-500 text-white py-2 px-4 rounded ml-2">Start mailing</button>
+        </div>
       </div>
     </div>
   );
