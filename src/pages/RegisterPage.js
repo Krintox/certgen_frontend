f9e5e8d3a7a8d3e2fc9e35e388332d976cad0038
SikariaKhushi <145835367+SikariaKhushi@users.noreply.github.com>
2024-03-04 07:27:44 +0000

    frontend changes

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -51,7 +51,7 @@ export default function RegisterPage() {
   
   
   const gradientBgLeft = {
-    background: "linear-gradient(to right, #FFA500, #FF6347)",
+    background: "linear-gradient(to bottom right, #FB360F, #F28A18)",
   };
 
   const borderOrange = {
@@ -59,21 +59,21 @@ export default function RegisterPage() {
   };
 
   const gradientBgRight = {
-    background: "linear-gradient(to right, #333333, #666666)",
+    background: "linear-gradient(to right, #222029, #3B4148)",
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex justify-center items-center h-screen w-3/4 ">
-        <div className="container mx-auto rounded-lg shadow-md relative flex overflow-hidden h-5/6 w-10/12">
+    <div className="flex justify-center items-center mt-12 mb-12">
+      <div className="flex justify-center items-center w-3/4 ">
+        <div className="container mx-auto rounded-lg shadow-md relative flex w-10/12">
           {/* Left Side */}
-          <div className="relative w-1/2 h-full flex flex-col rounded-lg" style={gradientBgLeft}>
+          <div className="relative w-1/2 flex flex-col rounded-lg" style={gradientBgLeft}>
             <div className='absolute left-[10%] flex flex-col'>
               <h1 className='text-4xl text-white font-bold mt-8 flex items-center z-10'>
                 <img src={logo} alt="logo" className="w-12 h-12 mr-2 mb-1" />
                 <span className="text-white">WEBKITES</span>
               </h1>
-              <p className='text-3xl text-white font-urbanist font-normal'>
+              <p className='text-3xl text-white font-urbanist font-normal pt-4'>
                 The only <br /> certificate <br /> automation <br /> tool you need
               </p>
             </div>
@@ -83,33 +83,32 @@ export default function RegisterPage() {
           </div>
 
           {/* Right Side */}
-          <div className="w-3/5 h-full flex flex-col justify-between items-center p-6 rounded-lg" style={gradientBgRight}>
+          <div className="w-1/2 flex flex-col justify-between items-center p-6 rounded-lg" style={{ ...gradientBgRight, flex: '1' }}>
             {/* Your existing right side content */}
             <div className='w-full flex flex-col max-w-[500px] bg-transparent'>
               <div className="w-full flex flex-col mb-2">
-                <h3 className="text-4xl font-semibold mb-10 text-center text-white mt-12 ">CREATE AN <span className="text-orange-500">ACCOUNT</span></h3>
+                <h3 className="text-5xl mb-10 text-center text-white mt-12 ">CREATE AN <span className="text-orange-500">ACCOUNT</span></h3>
               </div>
               <div className="rounded-lg p-4 mb-4 items-center text-center flex justify-center" style={borderOrange}>
                 <img src={googleIcon} alt="Google Icon" className="w-6 h-6 mr-2" />
-                <span className="text-white ">Sign up with Google</span>
+                <span className="text-white font-urbanist">Sign up with Google</span>
               </div>
-              <p className="text-white mb-4 text-center">- OR -</p>
+              <p className="text-white mb-4 text-center font-urbanist">- OR -</p>
               <form className="Register" onSubmit={Register}>
                 <input
                   type="text"
                   placeholder="Name"
                   value={username}
                   onChange={(ev) => setName(ev.target.value)}
-                  className="w-full text-white py-2 my-2 bg-transparent shadow-md outline-none focus:outline-none"
+                  className="text-white bg-transparent login-input focus:border-orange-400 font-urbanist mb-4"
                 />
-
 
                 <input
                   type="password"
                   placeholder="Password"
                   value={password}
                   onChange={(ev) => setPassword(ev.target.value)}
-                  className="w-full text-white py-2 my-2 bg-transparent shadow-md outline-none focus:outline-none"
+                  className="text-white bg-transparent login-input focus:border-orange-400 font-urbanist mb-4"
                 />
 
                 <input
@@ -117,22 +116,22 @@ export default function RegisterPage() {
                   placeholder="Confirm Password"
                   value={confirmPassword}
                   onChange={(ev) => setConfirmPassword(ev.target.value)}
-                  className="w-full text-white py-2 my-2 bg-transparent shadow-md outline-none focus:outline-none"
+                  className="text-white bg-transparent login-input focus:border-orange-400 font-urbanist mb-4"
                 />
 
-                <div className='w-full flex items-center justify-between'>
-                  <p className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2 text-white'>
+                <div className='w-full flex items-center justify-between mb-4'>
+                  <p className='text-sm font-medium whitespace-nowrap cursor-pointer text-white font-urbanist'>
                     Already have an account?{' '}
-                    <Link to="/login" className="text-orange font-semibold">
+                    <Link to="/login" className="text-orange font-urbanist">
                       Log in
                     </Link>
                   </p>
                 </div>
 
-                <div className='w-full flex flex-col my-4'>
+                <div className='w-full flex flex-col'>
                   <button
                     type="submit"
-                    className='w-full text-white my-2 font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
+                    className='w-full text-white my-2 rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
                     style={gradientBgLeft}>
                     Create Account
                   </button>
