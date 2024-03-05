5d61f2d58bd1646e8e0f6949956e256de169e834
SikariaKhushi <145835367+SikariaKhushi@users.noreply.github.com>
2024-03-05 15:04:59 +0530

    Gif fixed

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -4,7 +4,7 @@ import loadingGif from '../images/loading.GIF'; // Import your loading GIF
 
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
+    <div className="flex items-center justify-center bg-transparent">
       <img src={loadingGif} alt="Loading..." />
     </div>
   );
