d4f3ea2c5c70b99a41f617f8c69853fb3c03bf2f
SikariaKhushi <145835367+SikariaKhushi@users.noreply.github.com>
2024-03-24 23:42:02 +0530

    preview fixed and emailTemplate added

---
+++ src/pages/EmailTemplate.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+const EmailTemplate = () => {
+  const location = useLocation();
+  const [view, setView] = useState(null);
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+  const { state } = location;
+  const { uploadedExcelFile, annotations, resultImages, resultEmails } = location.state;
+  const [subLoad, setSubLoad] = useState(false);
+  const [selectedButton, setSelectedButton] = useState(null);
+
+
+  const handleViewChange = (selectedView) => {
+    setView(selectedView);
+    setSelectedButton(selectedView); // Set the selected button state
+  };
+
+  const handleProceed = async () => {
+    try {
+      const attachments = [];
+      setSubLoad(true);
+  
+      resultImages.forEach((base64String, index) => {
+        attachments.push({
+          filename: `image_${index + 1}.png`,
+          content: base64String,
+        });
+      });
+  
+      const emailData = {
+        subject: subject,
+        content: body,
+        recipients: resultEmails,
+        attachments: attachments,
+      };
+  
+      console.log('Email data:', emailData); // Log email data before sending request
+  
+      const response = await axios.post('https://certgen-backend.vercel.app/sendEmails', emailData);
+  
+      if (response.status === 200) {
+        setSubLoad(false);
+        alert('Emails sent successfully');
+        console.log('Emails sent successfully');
+      } else {
+        console.error('Failed to send emails');
+      }
+    } catch (error) {
+      console.error('Error sending emails:', error);
+    }
+  };
+  
+
+  const gradientBtn = {
+    background: selectedButton === 'labelled' ? "linear-gradient(to bottom right, #FB360F, #F28A18)" : "transparent", // Conditional background for labelled button
+    border: selectedButton === 'labelled' ? "none" : "1px solid white", 
+  };
+
+  const gradientBtnUnlabelled = {
+    background: selectedButton === 'unlabelled' ? "linear-gradient(to bottom right, #FB360F, #F28A18)" : "transparent", // Conditional background for unlabelled button
+    border: selectedButton === 'unlabelled' ? "none" : "1px solid white", 
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-w-screen m-5 min-h-screen">
+      <h1 className="text-7xl md:text-8xl font-semibold text-white border-b-2 under md:pb-2">CERT GEN</h1>
+      <div className="w-full bg-transparent rounded-lg shadow-md mt-10 p-4">
+        <div className="flex justify-center mb-8">
+          <button onClick={() => handleViewChange('labelled')} className="text-white py-2 px-4 border border-2 rounded cursor-pointer mr-4" style={gradientBtn}>
+            Labelled <span className="arrow">&#62;</span>
+          </button>
+          <button onClick={() => handleViewChange('unlabelled')} className="text-white py-2 border border-2 px-4 rounded cursor-pointer ml-4" style={gradientBtnUnlabelled}>
+            Unlabelled <span className="arrow">&#62;</span>
+          </button>
+        </div>
+        {view === 'labelled' && (
+          <div className="flex flex-col items-center justify-center mt-4 p-4">
+            <p className="text-md text-center font-medium text-white mb-4 font-urbanist">
+              Note: Use {'${Annotations}'} in the body to change if the said annotation is there in the body <br></br>For Example: “Congratulations {'${Name}'}” to put in the names.
+            </p>
+          </div>
+        )}
+        <div className="flex flex-col items-center justify-center border-2 border-solid border-orange-600 rounded-lg mt-4 p-8">
+          <div className="w-full mb-4 pl-8 pr-8">
+            <label htmlFor="subject" className="block bg-transparent text-white text-sm">
+              Subject:
+            </label>
+            <input
+              type="text"
+              id="subject"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-transparent leading-tight focus:outline-none focus:shadow-outline font-urbanist"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </div>
+          <div className="w-full mb-4 pl-8 pr-8">
+            <label htmlFor="body" className="block bg-transparent text-white text-sm mb-2">
+              Body:
+            </label>
+            <textarea
+              id="body"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-transparent leading-tight focus:outline-none focus:shadow-outline h-32 resize-none font-urbanist"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+          </div>
+          <button onClick={handleProceed} className="mt-4 bg-orange-500 text-white py-2 px-4 rounded cursor-pointer">
+            Proceed <span className="arrow">&#62;</span>
+          </button>
+        </div>
+      </div>
+      <style>
+        {`
+          .arrow {
+            font-size: 1.5rem;
+            margin-left: 0.5rem; 
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default EmailTemplate;
--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+<<<<<<< Updated upstream
 import { useLocation } from 'react-router-dom';
+=======
+import { useLocation, useNavigate } from 'react-router-dom';
+>>>>>>> Stashed changes
 import axios from 'axios';
 import JSZip from 'jszip';
 import Modal from 'react-modal';
@@ -22,6 +26,7 @@ const PreviewPage = () => {
   const [emailSubject, setEmailSubject] = useState('');
   const [emailContent, setEmailContent] = useState('');
   const [subLoad, setSubLoad] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     if (!isLoading && resultImages.length > 0 && resultEmails.length > 0) {
@@ -29,6 +34,7 @@ const PreviewPage = () => {
     }
   }, [isLoading, resultImages, resultEmails]);
 
+<<<<<<< Updated upstream
   const openEmailModal = () => {
     setIsEmailModalOpen(true);
   };
@@ -38,6 +44,8 @@ const PreviewPage = () => {
   };
 
 
+=======
+>>>>>>> Stashed changes
   const handleSendRequest = async () => {
     setIsLoading(true);
     if (annotations && resizedImage && uploadedExcelFile) {
@@ -135,32 +143,47 @@ const PreviewPage = () => {
       console.error('Error sending emails:', error);
     }
   };
-  
-  
-  
+
+  const handleImageClick = (base64String) => {
+    setSelectedImage(base64String);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedImage(null);
+  };
+
+  const gradientBtn = {
+    background: "linear-gradient(to bottom right, #FB360F, #F28A18)",
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="text-3xl md:text-5xl font-bold text-white border-b-2 under md:pb-2">CERT GEN - Preview</h1>
-      <div className="w-full max-w-2xl bg-transparent rounded-lg shadow-md mt-20 overflow-hidden">
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="w-full max-w-2xl bg-transparent rounded-lg shadow-md mt-20 overflow-hidden p-4">
+        <div className="flex justify-between">
+          <div className="w-1/2 pr-4">
+            <h1 className="text-5xl md:text-7xl font-bold text-white border-b-2 md:pb-2">CERT GEN</h1>
+          </div>
+          <div className="w-1/2">
+          <p className="text-white font-urbanist">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
         {isLoading ? (
           <LoadingComponent /> // Display the loading component here
-        ) : (
-          <>
-            <div className="flex justify-between">
-              <div className="w-full p-4">
-                <h2>Result Images</h2>
-                <div className="flex flex-wrap justify-center">
-                  {resultImages.slice(0, 5).map((base64String, index) => (
-                    <div key={index} className="m-2">
-                      <img
-                        src={`data:image/jpeg;base64,${base64String}`}
-                        alt={`Result Image ${index}`}
-                        style={{ maxWidth: '150px', maxHeight: '150px' }}
-                      />
-                    </div>
-                  ))}
+          ) : (
+            <>
+              {selectedImage && (
+                <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
+                  <div className="relative bg-white rounded-lg shadow-lg w-800px h-600px">
+                    <button
+                      className="absolute top-0 right-0 m-2 text-gray-600 hover:text-gray-800"
+                      onClick={handleClosePopup}
+                    >
+                      <IoMdClose size={24} />
+                    </button>
+                    <img src={`data:image/jpeg;base64,${selectedImage}`} alt="Selected Image" className="w-full h-full" /> {/* Adjust width and height here */}
+                  </div>
                 </div>
+<<<<<<< Updated upstream
               </div>
               <div className="w-1/2 p-4 text-white">
                 <h2>Result Emails</h2>
@@ -237,3 +260,67 @@ const PreviewPage = () => {
 
 export default PreviewPage;
 
+=======
+              )}
+              {showProceedButton ? (
+                <>
+                  <table className="w-full mt-14 mb-10">
+                  <thead>
+                    <tr>
+                      <th className="border-b-2 px-4 text-white font-urbanist py-2">Sl no</th>
+                      <th className="border-b-2 px-4 text-white font-urbanist py-2">Emails</th>
+                      <th className="border-b-2 px-4 text-white font-urbanist py-2">Click to preview</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {resultEmails.map((email, index) => (
+                      <tr key={index}>
+                        <td className="px-4 text-center text-white font-urbanist py-2">{index + 1}</td>
+                        <td className="px-4 text-center text-white font-urbanist py-2">{email}</td>
+                        <td className="px-4 text-center text-white font-urbanist py-2 flex justify-center">
+                          <button
+                            onClick={() => handleImageClick(resultImages[index])}
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
+                          >
+                            View
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+  
+                  <div className="w-full p-4 flex justify-center">
+                    <button
+                      onClick={handleProceed}
+                      className="text-white font-bold py-2 px-4 rounded"
+                      style={gradientBtn}
+                    >
+                      Start Mailing
+                    </button>
+                    <button
+                      onClick={handleBulkDownload}
+                      className="ml-4 text-white font-bold py-2 px-4 rounded"
+                      style={gradientBtn}
+                    >
+                      Download as ZIP format
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div className="w-full p-4 flex justify-center">
+                  <button onClick={handleSendRequest} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Generate
+                  </button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    );
+  };
+  
+  export default PreviewPage;
+  
+>>>>>>> Stashed changes
